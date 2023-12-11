06c22f04412b872d98391beb6859a85bfb8036c4
Valentyn P <valentinpodkopai@gmail.com>
2023-12-11 20:04:32 +0200

    Add config file with credentials for login tests

---
+++ config.py
@@ -0,0 +1,4 @@
+# Since it's opened for everyone in source code repo of website, we can use it as constants
+# But it's better to move it to env vars or any other credentials storage
+TEST_EMAIL = "customer@practicesoftwaretesting.com"
+TEST_PASS = "welcome01"
